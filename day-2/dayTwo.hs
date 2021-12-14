a8f34a3669b400cf079e16b4e14e51a31bd7d638
o-90 <john@demiurgemgmt.com>
2021-12-14 17:32:41 +0000

    do day two in haskell

---
+++ day-2/dayTwo.hs
@@ -0,0 +1,17 @@
+module Main where
+
+import System.Environment
+
+-- part one
+-- part two
+
+main :: IO ()
+main = do
+  [fname] <- getArgs
+  -- text <- readFile fname
+
+  -- compute part one
+  print "part one" 
+
+  -- compute part two
+  print "part two"
